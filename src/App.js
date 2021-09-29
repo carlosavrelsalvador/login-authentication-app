e22aac038d81b710cb6a9ca68fa7172928bdc27f
carlos vasquez <carlosavrelsalvador@gmail.com>
2021-09-29 17:51:34 -0600

    test deploy netlify

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <Router> 
+
+      <Switch>
+        <Route exact path="/">
+            <Login />
+         </Route> 
+         <Route  path="/login" component={Login} />
+         
+         <Route path="/dashboard">
+            <Dashboard />
+         </Route> 
+
+          {/* Ruta para algo not found */}
+         <Route component={NotFound} />
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
